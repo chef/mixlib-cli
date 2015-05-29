afc46648cf3e8803bbe7bb0722b188d7435737c9
danielsdeleo <dan@getchef.com>
2015-05-29 09:59:27 -0700

    Add basic rspec configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,3 +7,10 @@ class TestCLI
   include Mixlib::CLI
 end
 
+RSpec.configure do |config|
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.warnings = true
+end
+
